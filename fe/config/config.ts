4652e1578af0c2fa69d83e282e3d797dac358c9e
Oliver Veras <55413049+acoymess@users.noreply.github.com>
2022-02-03 13:43:39 -0500

    config changes for prod

--- fe/config/config.ts
+++ fe/config/config.ts
@@ -3,7 +3,7 @@ type Config = {
 }
 
 const dev: string = 'http://localhost:5000/graphql'
-const prod: string = '<not yet>'
+const prod: string = 'https://boo-dash.herokuapp.com/graphql'
 
 const config: Config = {
   baseUrl: process.env.NODE_ENV === 'development' ? dev : prod,
